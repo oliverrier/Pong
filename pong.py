530bec60b92940b8218eb76825456023888eeda7
Olivier Verrier <olivier.verrier@ynov.com>
2019-01-06 10:52:22 +0100

    Added score
    Score still bugged

--- pong.py
+++ pong.py
@@ -17,12 +17,20 @@ class Ball ():
 
     def move_ball(self):
         position = self.screen.coords(self.ball)
-        if position[0] <= 0 or position[2] >= self.screen_width:
-            self.deltax = - self.deltax
+        if position[0] <= 0:
+            player_2.score += 1
+            print(player_2.score)
+            position = [self.x1, self.y1, self.x2, self.y2]
+        if position[2] >= screen_width:
+            player_1.score += 1
+            print(player_1.score)
+            position = [self.x1, self.y1, self.x2, self.y2]
         if position[1] <= 0 or position[3] >= self.screen_height:
             self.deltay = - self.deltay
         self.screen.move(self.ball, self.deltax, self.deltay)
         self.screen.after(20, self.move_ball)
+            
+        
 
 
 class Player():
@@ -30,19 +38,20 @@ class Player():
         self.screen = game_screen
         self.screen_width = screen_width
         self.screen_height = screen_height
-        self.deltay = 3
+        self.deltay = 25
+        self.score = 0
         self.paddle = game_screen.create_rectangle(
             position[0], position[1], position[2], position[3], fill='white')
-    
+
     def move_paddle_up(self):
-        postion = self.screen.coords(self.paddle)
-        if position[1] == 0:
+        paddle_position = self.screen.coords(self.paddle)
+        if paddle_position[1] == 0:
             self.deltay = 0
         self.screen.move(self.paddle, 0, - self.deltay)
 
     def move_paddle_down(self):
-        postion = self.screen.coords(self.paddle)
-        if postion[3] == self.screen_height:
+        paddle_postion = self.screen.coords(self.paddle)
+        if paddle_postion[3] == self.screen_height:
             self.deltay = 0
         self.screen.move(self.paddle, 0, self.deltay)
 
@@ -56,11 +65,18 @@ game_screen = Canvas(screen, width=screen_width,
                      height=screen_height, bg='black')
 game_ball = Ball(game_screen, screen_width/2 - 5, screen_height /
                  2 - 5, screen_width/2 + 5, screen_height/2 + 5)
-player_1 = Player(game_screen, [10, screen_height/2 - 25, 20, screen_height/2 + 25])
-player_2 = Player(game_screen, [screen_width - 20, screen_height/2 - 25, screen_width - 10, screen_height/2 +25])
+player_1 = Player(
+    game_screen, [10, screen_height/2 - 25, 20, screen_height/2 + 25])
+player_2 = Player(game_screen, [
+                  screen_width - 20, screen_height/2 - 25, screen_width - 10, screen_height/2 + 25])
+game_screen.bind("z", player_1.move_paddle_up)
+game_screen.bind("s", player_1.move_paddle_down)
+game_screen.bind("<Up>", player_2.move_paddle_up)
+game_screen.bind("<Down>", player_2.move_paddle_down)
 game_screen.pack()
 
 # Component's placements
+
 game_screen.grid(row=0, column=0, rowspan=2, columnspan=3)
 
 
